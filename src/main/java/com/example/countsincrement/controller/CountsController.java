f351c009bb1da53237d65091723188c9133bbb75
abhishek-amwater <Abhishek.Tomar@amwater.com>
2021-06-07 20:55:33 +0530

    Handling Some Exception

--- src/main/java/com/example/countsincrement/controller/CountsController.java
+++ src/main/java/com/example/countsincrement/controller/CountsController.java
@@ -6,26 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class CountsController {
-    static long count = 0;
+    static long initCount = 0;
 
     @Autowired
     private CountsRepo countsRepo;
 
     @GetMapping("count")
     public synchronized long getCount(Counts counts) {
-      Optional<Counts> c = countsRepo.findById(0);
-        count= c.get().getCount() +1;
-        counts.setCount(count);
-        if(count<=1)
+        try {
+            Optional<Counts> c = countsRepo.findById(0);
+            initCount= c.get().getCount() +1;
+            counts.setCount(initCount);
+            countsRepo.updateCount(0,initCount);
+        }
+        catch(Exception e){
+            initCount=1;
+            counts.setCount(initCount);
             countsRepo.save(counts);
-        else
-            countsRepo.updateCount(0,count);
-        return count;
+        }
+        return initCount;
     }
 
 }
